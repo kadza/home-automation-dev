2ac2219c1110546a55c2f5cabd92fa031551faab
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-11-06 11:31:07 +0000

    ssh keys from secret

---
+++ admin-clean-rebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd node-red-admin
+./deploy.sh
+cd ..
+docker stop node-red-admin && docker rm node-red-admin
+sudo rm -Rf ~/node-red/data/ && mkdir ~/node-red/data  && sudo chown -R 1000:1000 ~/node-red/data
+/usr/local/bin/docker-compose up -d
